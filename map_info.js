ebdd1ad1ef0ab4fd514df81dc68bf7d4a4ce7d74
Ice Dodo <75837696+icedodo@users.noreply.github.com>
2020-12-11 12:48:42 +0200

    Create map_info.js

---
+++ map_info.js
@@ -0,0 +1,150 @@
+var cup_info = {
+	'ice': { //  - Beginner
+		name: "Ice Cup",
+		desc: "Fun maps for starters.",
+		color: "#41B3BF"
+	},
+	'cone': { //  - Intermediate
+		name: "Cone Cup",
+		color: "#F2A057"
+	},
+	'fire': { //  - Challenger
+		name: "Fire Cup",
+		color: "#F24949"
+	}
+}
+
+var map_info = {
+	'ice': [
+		{
+			name: 'Welcome Map',
+			id: "tut1",
+			diff: "1"
+		},
+		{
+			name: 'Icy Path',
+			id: "ice1",
+			diff: "2"
+		},
+		{
+			name: 'Snowboarder',
+			id: "ice2",
+			diff: "2"
+		},
+		{
+			name: 'Mountain',
+			id: "beardedbaby",
+			diff: "2"
+		},
+		{
+			name: "Scorpion",
+			id: "scorpion",
+			diff: "2"
+		},
+		{
+			name: "Easy Drifting",
+			id: "easydrifting",
+			diff: "3"
+		},
+		{
+			name: 'Hills',
+			id: "hills",
+			diff: "3"
+		},
+		{
+			name: 'Twisted Road',
+			id: "twistedroad",
+			diff: "4"
+		}
+	],
+	'cone': [
+		{
+			name: 'Dangerous Zone',
+			id: "cone3",
+			diff: "4"
+		},
+		{
+			name: 'The Red Hats',
+			id: "cone2",
+			diff: "4"
+		},
+		{
+			name: 'Turning Challenge',
+			id: "sungjoon",
+			diff: "3"
+		},
+		{
+			name: 'Apartment',
+			id: 'apartment',
+			diff: "4"
+		},
+		{
+			name: 'Secret Tunnel',
+			id: 'secrettunnel',
+			diff: "4"
+		},
+		{
+			name: 'Space Test',
+			id: 'spacetest',
+			diff: "4"
+		},
+		{
+			name: 'Cross Gravity',
+			id: 'crossgravity',
+			diff: "4"
+		},
+		{
+			name: 'Winding Path',
+			id: 'windingpath',
+			diff: "3"
+		}
+	],
+	'fire': [
+		{
+			name: '4D Demon Cinema',
+			id: "cone1",
+			diff: "5"
+		},
+		{
+			name: 'Conefield',
+			id: "conefield",
+			diff: "4"
+		},
+		{
+			name: 'Space Canyon',
+			id: "spacecanyon",
+			diff: "5"
+		},
+		{
+			name: 'Coney Cliffs',
+			id: "coneycliffs",
+			diff: "4"
+		},
+		{
+			name: 'Everest',
+			id: "everest",
+			diff: "5"
+		},
+		{
+			name: "0001",
+			id: "0001",
+			diff: "7"
+		},
+		{
+			name: 'Project Stardust',
+			id: "stardust",
+			diff: "5"
+		},
+		{
+			name: "Cross Path",
+			id: "crosspath",
+			diff: "4"
+		},
+		{
+			name: "Red Lights Green Lights",
+			id: "redlights",
+			diff: "4"
+		}
+
+	]
+}
